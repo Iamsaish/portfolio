8595829fc1185c2772e269bed6531355bac51083
Saish Shah <shahsaish@gmail.com>
2020-09-28 13:40:35 +0530

    Updates

---
+++ precache-manifest.f3079602caeb36fa45639899d40b3674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5c29d10771f96930390d038f59fd63",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "243c0c436ba5565eb651",
+    "url": "/portfolio/static/css/main.10b0c898.chunk.css"
+  },
+  {
+    "revision": "6850aaa938de7f84962b",
+    "url": "/portfolio/static/js/2.c0f747f4.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/portfolio/static/js/2.c0f747f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243c0c436ba5565eb651",
+    "url": "/portfolio/static/js/main.1c5814a4.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
